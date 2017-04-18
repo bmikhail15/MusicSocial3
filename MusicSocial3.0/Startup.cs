600a8045a833bf339869e796f53dab3b8dd9dca1
Jessie Simpauco <jessie_simpauco@live.com>
2017-04-17 21:46:28 -0400

    Add project files.

---
+++ MusicSocial3.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicSocial3._0.Startup))]
+namespace MusicSocial3._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
