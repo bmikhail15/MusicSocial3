a4c2c21cbfdb6748377e569c9a9a739f859174f7
Jessie Simpauco <jessie_simpauco@live.com>
2017-04-22 15:35:37 -0400

    Added Roles & Use Controllers

---
+++ MusicSocial3.0/Migrations/201704190414265_Add UserRole.cs
@@ -0,0 +1,16 @@
+namespace MusicSocial3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserRole : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
